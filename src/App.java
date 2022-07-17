f80f281f2c1e77a345c4d4eb87fd836bc52b65c9
EmersonLaranja <emersonlaranja@gmail.com>
2022-07-17 05:06:15 -0300

    Aula 2

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -9,7 +11,6 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) throws Exception {
         // create HTTP connection and search the top 250 movies
-        System.out.println("OI");
         String url = "https://imdb-api.com/en/API/Top250Movies/k_zmuid1iv";
 
         URI address = URI.create(url);
@@ -26,10 +27,18 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
         // show data info for each movie
+        var geradora = new GeradoraDeFigurinhas();
+
         for (Map<String, String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
             System.out.println();
         }
 
